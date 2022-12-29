769a2fe2b14c327a87de62718def5d5917b59fb6
starydv7 <pawanstar72926@gmail.com>
2022-12-29 23:00:22 +0530

    data

--- src/components/Context.js
+++ src/components/Context.js
@@ -18,14 +18,24 @@ const ProductContext = React.createContext();
      onEdit = (id) => {
          const tempProduct = this.state.AllData;
          const index = tempProduct.indexOf(this.getRecord(id));
-         const selectProduct = tempProduct[index];
+         const selectRecord = tempProduct[index];
+         this.setState({
+             id: selectRecord['id'],
+             title: selectRecord['title'],
+             info: selectRecord['info'],
+             price: selectRecord['price'],
+             company:selectRecord['company']
+             
+         })
      }
     render() {
         console.log(this.state.AllData);
     return (
         <div>
             <ProductContext.Provider
-            value={{...this.state}}>
+                value={{
+                    ...this.state,
+                onEdit:this.onEdit}}>
 {this.props.children}
             </ProductContext.Provider>
       </div>
