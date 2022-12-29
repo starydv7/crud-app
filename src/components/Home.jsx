08f0e470536cb899c8c799a3d89d1548edb78b86
starydv7 <pawanstar72926@gmail.com>
2022-12-29 16:36:50 +0530

    fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,57 @@
-import React, { Component } from 'react'
-import { ProductConsumer } from './Context'
+import React, { Component } from "react";
+import { ProductConsumer } from "./Context";
 export default class Home extends Component {
   render() {
     return (
-        <div>
-            <h3>Crud operation</h3>
-            <ProductConsumer>
-                {(value) => {
+      <div>
+        <h3>Crud operation</h3>
+        <ProductConsumer>
+          {(value) => {
+            return (
+              <table>
+                <tbody>
+                  <tr>
+                    <th>Title</th>
+                    <th>Info</th>
+                    <th>Price</th>
+                    <th>Company</th>
+                    <th>Actions</th>
+                  </tr>
+                  {value.AllData.map((item) => {
                     return (
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th>Title</th>
-                                    <th>Info</th>
-                                    <th>Price</th>
-                                    <th>Company</th>
-                                    <th>Actions</th>
-                                </tr>
-                                {value.AllData.map((item) => {
-                                    return (
-                                      <tr>
-                                        <td>{item.title}</td>
-                                        <td>{item.info}</td>
-                                        <td>{item.price}</td>
-                                            <td>{item.company}</td>
-                                            <td>{item.inCart.toString()}</td>
-                                            <td> 
-                                                <button onClick={() => { value.onEdit() }}>
-Edit
-                                                </button>
-                                            </td>
-                                      </tr>
-                                    );
-                               })} 
-                            </tbody>
-                        </table>
-                    )
-                }}
-            </ProductConsumer> 
-
-
-
-
+                      <tr>
+                        <td>{item.title}</td>
+                        <td>{item.info}</td>
+                        <td>{item.price}</td>
+                        <td>{item.company}</td>
+                        <td>{item.inCart.toString()}</td>
+                        <td>
+                          <button
+                            onClick={() => {
+                              value.onEdit();
+                            }}
+                          >
+                            Edit
+                          </button>
+                        </td>
+                        <td>
+                          <button
+                            onClick={() => {
+                              value.onEdit();
+                            }}
+                          >
+                            Edit
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            );
+          }}
+        </ProductConsumer>
       </div>
-    )
+    );
   }
 }
